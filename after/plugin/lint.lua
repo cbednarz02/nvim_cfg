99b40001ea5f649b7f7ff8b94ff734449a7f722d
Christopher Bednarz <christopherbednarz@Christophers-MacBook-Pro.local>
2024-08-08 23:20:38 -0400

    enable move line shortcuts disable cspell

--- after/plugin/lint.lua
+++ after/plugin/lint.lua
@@ -1,6 +1,6 @@
 
 require('lint').linters_by_ft = {
-  markdown = {'cspell'}
+--  markdown = {'cspell'}
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
@@ -12,7 +12,7 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 
     -- You can call `try_lint` with a linter name or a list of names to always
     -- run specific linters, independent of the `linters_by_ft` configuration
-    require("lint").try_lint("cspell")
+ --   require("lint").try_lint("cspell")
   end,
 })
 
--- lua/vim_keymap.lua
+++ lua/vim_keymap.lua
@@ -1,3 +1,5 @@
+local map = vim.keymap.set
+
 vim.opt.nu = true
 vim.opt.tabstop = 8
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
@@ -9,4 +11,15 @@ vim.opt.list = true
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+
+-- disable these as it is hard to nagivate vertically on mac.
+--map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+--map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
