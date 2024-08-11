b771d91735adf785a62911b31c84ce4e3f9aabec
cbednarz02 <cbednarz02@gmail.com>
2024-08-11 11:42:22 -0400

    move lsp-zero to 4.x

--- lua/plugins/initial.lua
+++ lua/plugins/initial.lua
@@ -12,19 +12,25 @@ return {
 	},
 	'mbbill/undotree',
 	'tpope/vim-fugitive',
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		dependencies = {
-		--- Uncomment the two plugins below if you want to manage the language servers from neovim
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
-			{'neovim/nvim-lspconfig'},
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	},
+--	{
+--		'VonHeikemen/lsp-zero.nvim',
+--		branch = 'v3.x',
+--		dependencies = {
+--		--- Uncomment the two plugins below if you want to manage the language servers from neovim
+--			{'williamboman/mason.nvim'},
+--			{'williamboman/mason-lspconfig.nvim'},
+--			{'neovim/nvim-lspconfig'},
+--			{'hrsh7th/nvim-cmp'},
+--			{'hrsh7th/cmp-nvim-lsp'},
+--			{'L3MON4D3/LuaSnip'},
+--		}
+--	},
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
 	'mfussenegger/nvim-lint',
 	'lewis6991/gitsigns.nvim',
 }
